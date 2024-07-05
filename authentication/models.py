7a306fbbf086847c2c3b36b9cf561415d2952220
Joseph Orokpo <josephorokpo@gmail.com>
2024-07-05 16:05:57 +0100

    Initialize project models

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        
+        if extra_fields.get('is_active') is not True:
+            raise ValueError(_('Superuser must have is_active=True.'))
+        
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    username = models.CharField(_('Username'), max_length=50, unique=True, default='default_username')
+    email = models.EmailField(_('Email'), max_length=60, unique=True)
+    phone_number = PhoneNumberField(unique=True, null=False, blank=False)
+    date_joined = models.DateTimeField(_('Date'), auto_now_add=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'phone_number']
+    
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
