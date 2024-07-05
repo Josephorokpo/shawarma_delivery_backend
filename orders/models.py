6d909b9422157040d17955eab7b7795abebdba0a
Joseph Orokpo <josephorokpo@gmail.com>
2024-07-05 19:43:52 +0100

    Initialize orders models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Order(models.Model):
+    SIZES = (
+        ('SMALL', 'Small'),
+        ('MEDIUM', 'Medium'),
+        ('LARGE', 'Large'),
+        ('EXTRA_LARGE', 'Extra Large'),
+    )
+  
+    ORDER_STATUS = (
+        ('PENDING', 'Pending'),
+        ('IN_TRANSIT', 'In Transit'),
+        ('DELIVERED', 'Delivered'),
+    )
+    
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    size = models.CharField(max_length=30, choices=SIZES, default=SIZES[0][0])
+    status = models.CharField(max_length=30, choices=ORDER_STATUS, default=ORDER_STATUS[0][0])
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+  
+    def __str__(self):
+        return f"<Order {self.size} by {self.customer.id}>"
