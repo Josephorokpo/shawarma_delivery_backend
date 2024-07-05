63a56655f86280936045bacce885c8fe6003d989
Joseph Orokpo <josephorokpo@gmail.com>
2024-07-05 14:35:05 +0100

    Create authentication views file

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloAuthView(generics.GenericAPIView):
+    def get(self, request):
+        response_data = {"message": "hello Auth"}
+        return Response(data=response_data, status=status.HTTP_200_OK)
