ccc52dd4e87e47c836d1613a8ba9b1a9c7e08a02
Joseph Orokpo <josephorokpo@gmail.com>
2024-07-08 11:46:09 +0100

    Add endpoint for UserOrderDetailView to retrieve specific order details for a user

--- orders/urls.py
+++ orders/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
     path('', views.OrderCreateListView.as_view(), name='order_list_create'),
     path('<int:id>/', views.OrderDetailView.as_view(), name='order_detail'),
     path('<int:id>/status/', views.UpdateOrderStatusView.as_view(), name='order_status_update'),
     path('user/<int:user_id>/orders/', views.UserOrdersView.as_view(), name='user_orders'),
+    path('user/<int:user_id>/orders/<int:order_id>/', views.UserOrderDetailView.as_view(), name='user_order_detail'),
 ]
