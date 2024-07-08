f9ec9e2f45d77c36fb06e46d8cc8c7758cafc9d9
Joseph Orokpo <josephorokpo@gmail.com>
2024-07-08 11:31:08 +0100

    Add URL route for listing orders by user ID in UserOrdersView

--- orders/urls.py
+++ orders/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.OrderCreateListView.as_view(), name='order_list_create'),
     path('<int:id>/', views.OrderDetailView.as_view(), name='order_detail'),
     path('<int:id>/status/', views.UpdateOrderStatusView.as_view(), name='order_status_update'),
+    path('user/<int:user_id>/orders/', views.UserOrdersView.as_view(), name='user_orders'),
 ]
