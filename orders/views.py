bd070e3cc976623edf0a0f615b51b0c89d02794a
Joseph Orokpo <josephorokpo@gmail.com>
2024-07-08 11:52:48 +0100

    Imported get_user_model() from django.contrib.auth for user model retrieval

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
 from .serializers import OrderSerializer, OrderStatusUpdateSerializer, UserOrderSerializer
 from .models import Order
+
+
+User = get_user_model()
 
 # Create your views here.
 
