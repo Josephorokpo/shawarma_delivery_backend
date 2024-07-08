817a75667476696d30bb87b358d67ae055cf27a8
Joseph Orokpo <josephorokpo@gmail.com>
2024-07-08 11:06:57 +0100

    Create OrderCreateListView to handle listing and creating orders with appropriate permissions

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
+from .serializers import OrderSerializer, OrderStatusUpdateSerializer, UserOrderSerializer
+from .models import Order
 
 # Create your views here.
-class HelloOrderView(generics.GenericAPIView):
-    def get(self, request):
-        response_data = {"message": "hello Orders"}
-        return Response(data=response_data, status=status.HTTP_200_OK)
+
+class OrderCreateListView(generics.ListCreateAPIView):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+     
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(customer=self.request.user)
+
