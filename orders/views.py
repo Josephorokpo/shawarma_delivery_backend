fce914257c01ee532384b7cd4ae0982c10adec9c
Joseph Orokpo <josephorokpo@gmail.com>
2024-07-08 11:11:50 +0100

    Add OrderDetailView for retrieving, updating, and deleting orders

--- orders/views.py
+++ orders/views.py
@@ -23,3 +23,27 @@ class OrderCreateListView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(customer=self.request.user)
 
+class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+    permission_classes = [IsAdminUser]
+    lookup_field = 'id'
+
+     
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+     
+    def put(self, request, id):
+        order = self.get_object()
+        serializer = self.serializer_class(order, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+     
+    def delete(self, request, id):
+        order = self.get_object()
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
