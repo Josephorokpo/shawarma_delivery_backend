e65a2ab884e4741448a2df60935fc8a67b7831e4
Joseph Orokpo <josephorokpo@gmail.com>
2024-07-08 11:05:19 +0100

    Add serializers for Order model including OrderSerializer, OrderStatusUpdateSerializer, and UserOrderSerializer

---
+++ orders/serializers.py
@@ -0,0 +1,23 @@
+
+from .models import Order
+from rest_framework import serializers
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'size', 'status', 'quantity', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'customer', 'created_at', 'updated_at']
+        
+        
+class OrderStatusUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'size', 'status', 'quantity', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'customer', 'created_at', 'updated_at']
+        
+        
+class UserOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'size', 'status', 'quantity', 'created_at', 'updated_at']
