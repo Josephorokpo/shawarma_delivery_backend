970dfdfaa765dfe6b06ed7b0df50c88ab7954bca
Joseph Orokpo <josephorokpo@gmail.com>
2024-07-08 16:20:54 +0100

    Update settings.py

--- shawarma_delivery/settings.py
+++ shawarma_delivery/settings.py
@@ -55,6 +55,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
     'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'NON_FIELD_ERRORS_KEY': 'error',
 }
 
 SIMPLE_JWT = {
